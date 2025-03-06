a0592bfb930c51fb46438892f7f50ae7c36cc4cb
shubhamroy1996 <shubham.roy1996.sr@gmail.com>
2025-03-06 23:03:53 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <div>
+      <Button>Login</Button>
+    </div>
+  )
+}
+
+export default App
